bc81dbd19b2bac404479e93d7a2e0ebe019f2d25
Cillian G <cilliangoan221@gmail.com>
2022-09-10 19:41:14 +0000

    Add urlpattern for location pages

--- guide/urls.py
+++ guide/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PreviewList.as_view(), name='home')
+    path('', views.PreviewList.as_view(), name='home'),
+    path('<slug:slug>/', views.LocationDetails.as_view(), name='location')
 ]
