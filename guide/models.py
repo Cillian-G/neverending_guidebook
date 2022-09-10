a60dbbb7c6bd97a75342b5226a5361970d31a2bd
Cillian G <cilliangoan221@gmail.com>
2022-09-10 13:58:56 +0000

    Add Newsletter, Patron, Contact models

--- guide/models.py
+++ guide/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField 
 
+
 class Location(models.Model):
 
     COUNTRY_CHOICES = [
@@ -33,3 +34,36 @@ class Location(models.Model):
     def __str__(self):
         return self.title
 
+
+class Newsletter(models.Model):
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.email
+
+
+class Contact(models.Model):
+
+    SUBJECT_CHOICES = [
+        ('account_issues', 'request help with your account'),
+        ('submission_request', 'writing for us'),
+        ('correction_request', 'correct one of our location listings'),
+        ('location_request', 'suggest an location for us to visit'),
+        ('non_cateorized', 'other purposes')
+    ]
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    subject = models.CharField(choices=SUBJECT_CHOICES)
+    message = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Patron(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    patron_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user
