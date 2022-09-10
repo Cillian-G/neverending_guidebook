3396baddd6eeff84de648ceeebd64bcba8ecf642
Cillian G <cilliangoan221@gmail.com>
2022-09-10 12:27:59 +0000

    Add Location model

--- guide/models.py
+++ guide/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField 
 
-# Create your models here.
+class Location(models.Model):
+
+    COUNTRY_CHOICES = [
+        ('BE', 'Belize'),
+        ('CR', 'Costa Rica'),
+        ('ES', 'El Salvador'),
+        ('GU', 'Guatemala'),
+        ('HO', 'Honduras'),
+        ('NI', 'Nicaragua'),
+        ('PA', 'Panama')
+    ]
+
+    REGION_CHOICES = [
+        ('CA', 'Central America')
+    ]
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    country = models.CharField(choices=COUNTRY_CHOICES)
+    region = models.CharField(choices=REGION_CHOICES)
+    content = models.TextField(blank=True)
+    bookmarks = models.ManyToManyField(
+        User, related_name='location_bookmark', blank=True
+    )
+    map_image = CloudinaryField('image', default='placeholder')
+    landscape_image = CloudinaryField('image', default='placeholder')
+    feature_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.title
+
