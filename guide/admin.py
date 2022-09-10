a4492bb004284fc653ce8b8f985cdbca024c9f43
Cillian G <cilliangoan221@gmail.com>
2022-09-10 15:01:05 +0000

    Configure Location and Patron admin pages

--- guide/admin.py
+++ guide/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Region, Location, Newsletter, Contact, Patron  
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+admin.site.register(Region)
+# @admin.register(Region)
+# class RegionAdmin(admin.ModelAdmin):
+
+#     list_display = ('name')
+
+
+@admin.register(Location)
+class LocationAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'region', 'country', 'preview')
+    search_fields = ['title', 'content']
+    list_filter = ('preview', 'region')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+    
+
+@admin.register(Patron)
+class PatronAdmin(admin.ModelAdmin):
+    list_display = ('user', 'patron_status')
+    list_filter = ('patron_status',)
+    search_fields = ('user',)
