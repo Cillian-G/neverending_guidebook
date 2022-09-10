0dfc7bb61e10a95af9e9a5938ad9fd230f19e9f6
Cillian G <cilliangoan221@gmail.com>
2022-09-10 20:24:36 +0000

    Add view for location details

--- guide/views.py
+++ guide/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Location   
 
 
@@ -7,3 +7,22 @@ class PreviewList(generic.ListView):
     model = Location
     queryset = Location.objects.filter(preview=True).order_by('title')
     template_name = 'index.html'
+
+
+class LocationDetails(View):
+
+    def get(self, request, slug, *args, **kwargs): 
+        queryset = Location.objects
+        location = get_object_or_404(queryset, slug=slug)
+        bookmarked = False
+        if location.bookmarks.filter(id=self.request.user.id).exists():
+            bookmarked = True
+        
+        return render(
+            request,
+            "location.html",
+            {
+                'location': location,
+                'bookmarked': bookmarked,
+            }
+        )
