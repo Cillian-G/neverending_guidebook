4f5931795451439d82eaff870782a3536e819fc6
Cillian G <cilliangoan221@gmail.com>
2022-09-11 19:12:35 +0000

    Add patron_status field to user model admin page

--- guide/admin.py
+++ guide/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import Region, Location, Newsletter, Contact, Patron  
 from django_summernote.admin import SummernoteModelAdmin
+
 
 admin.site.register(Region)
 # @admin.register(Region)
@@ -24,3 +27,13 @@ class PatronAdmin(admin.ModelAdmin):
     list_display = ('user', 'patron_status')
     list_filter = ('patron_status',)
     search_fields = ('user',)
+
+class PatronInline(admin.StackedInline):
+    model = Patron
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (PatronInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
