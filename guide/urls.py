460238677ec91985ca7285f8c36bae7a83cf1fa3
Cillian G <cilliangoan221@gmail.com>
2022-09-11 19:14:36 +0000

    Fix directory urlpattern bug

--- guide/urls.py
+++ guide/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PreviewList.as_view(), name='home'),
-    path('<slug:slug>/', views.LocationDetails.as_view(), name='location'),
     path('directory/', views.LocationList.as_view(), name='directory'),
+    path('<slug:slug>/', views.LocationDetails.as_view(), name='location'),
+    
 ]
