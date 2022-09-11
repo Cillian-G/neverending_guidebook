9c5155ec43a681c2cd7345425c9030d41a41753d
Cillian G <cilliangoan221@gmail.com>
2022-09-11 14:51:44 +0000

    Set up allauth, add allauth navbar links

--- guidebook/urls.py
+++ guidebook/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('guide.urls'), name='guide_urls')
+    path('', include('guide.urls'), name='guide_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
