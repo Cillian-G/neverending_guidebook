6981cc153aa857c1e6fa45095860e962ebecdf6f
Cillian G <cilliangoan221@gmail.com>
2022-09-15 09:12:14 +0000

    Add checkout Forms, models, urls and views

--- checkout/models.py
+++ checkout/models.py
@@ -10,3 +10,4 @@ class PatronUpgrade(models.Model):
 
     date = models.DateTimeField(auto_now_add=True)
     email = models.EmailField(max_length=254, null=False, blank=False)
+    country_of_residence = models.CharField(max_length=40, null=False, blank=False)
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import CheckoutForm
 
-# Create your views here.
+# Add check for patron status
+def checkout(request):
+    patron_status = False
+    if patron_status:
+        messages.error(request, "Your account is already patron status")
+        return redirect(reverse('home'))
+    
+    #unauthenticated users wont be able to see the form, so any users without accounts that access this page
+    #will get an error message in html
+
+    checkout_form = CheckoutForm()
+    template = 'upgrade.html'
+    context = {
+        'checkout_form': checkout_form,
+        'stripe_public_key': 'pk_test_51Li1KkBsxJQ4yPXp9RXMBfi2sDkcB9QujqRKv7ldCCvWxtVtC5caguO9WeEL5Gj8OEV4NugbASdrr4NYDSNRSOlf007uvO8Ydn',
+        'client_secret': 'test_client_secret'
+    }   
+
+    return render(request, template, context)
