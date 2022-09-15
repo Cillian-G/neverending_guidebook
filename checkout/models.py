8d7a258d192951990cdeb5c32de2d0215c8a38bd
Cillian G <cilliangoan221@gmail.com>
2022-09-15 14:54:10 +0000

    Add order number functionality

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.conf import settings
 
@@ -8,6 +10,20 @@ from guide.models import Patron
 
 class PatronUpgrade(models.Model):
 
+    order_number = models.CharField(max_length=32, null=False, editable=False)
     date = models.DateTimeField(auto_now_add=True)
     email = models.EmailField(max_length=254, null=False, blank=False)
     country_of_residence = models.CharField(max_length=40, null=False, blank=False)
+
+    def _generate_order_number(self):
+        
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.order_number
