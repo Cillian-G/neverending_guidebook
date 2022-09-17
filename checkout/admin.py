3f789be6fc603ebb0d1784df822f26ab8a47fec6
Cillian G <cilliangoan221@gmail.com>
2022-09-17 21:05:19 +0000

    Set up admin for contact and checkout apps

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
 from .models import PatronUpgrade
 
 
-# Register your models here.
-# @admin.register(PatronUpgrade)
-# class PatronAdmin(admin.ModelAdmin):
-#     list_display = ('email', 'date', 'country of residence')
-#     list_filter = ('patron_status',)
-#     search_fields = ('user',)
+@admin.register(PatronUpgrade)
+class PatronUpgradeAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date', 'order_number')
+    list_filter = ('date',)
+    search_fields = ('email', 'order_number')
--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject')
+    list_filter = ('subject',)
+    search_fields = ('message',)
