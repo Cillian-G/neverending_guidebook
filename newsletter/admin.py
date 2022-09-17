dd904cdecb80b5080b44176ffb58a91d26a8d3b2
Cillian G <cilliangoan221@gmail.com>
2022-09-17 21:06:16 +0000

    Set up admin for newsletter app

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Newsletter
 
-# Register your models here.
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+    list_filter = ('email',)
+    search_fields = ('email',)
