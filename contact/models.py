c86c3631aa63c8b72e10bfeb7d1d27c27ff13f21
Cillian G <cilliangoan221@gmail.com>
2022-09-17 18:11:50 +0000

    Add contact form functionality

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+
+    SUBJECT_CHOICES = [
+        ('account_issues', 'request help with your account'),
+        ('submission_request', 'writing for us'),
+        ('correction_request', 'correct one of our location listings'),
+        ('location_request', 'suggest an location for us to visit'),
+        ('non_cateorized', 'other purposes')
+    ]
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    subject = models.CharField(max_length=20, choices=SUBJECT_CHOICES)
+    message = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.views import generic, View
+from .forms import ContactForm
 
-# Create your views here.
+def contact(request):
+
+    if request.method == 'POST':
+
+        form_data = {
+            'email': request.POST['email'],
+            'name': request.POST['name'],
+            'subject': request.POST['subject'],
+            'message': request.POST['message']
+            }
+        
+        contact_form = ContactForm(form_data)
+        if contact_form.is_valid():
+            contact_form.save()
+            return render(
+                request,
+                "contact_success.html",)
+
+    contact_form = ContactForm()
+    template = 'contact.html'
+    context = {
+        'contact_form': contact_form,
+    }
+    return render(request, template, context)
