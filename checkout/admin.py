303414ca4b31b31a4efab1ecc7fb08c4b863dc1a
Cillian G <cilliangoan221@gmail.com>
2022-09-17 13:09:45 +0000

    Add commented out placeholder code

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import PatronUpgrade
+
 
 # Register your models here.
+# @admin.register(PatronUpgrade)
+# class PatronAdmin(admin.ModelAdmin):
+#     list_display = ('email', 'date', 'country of residence')
+#     list_filter = ('patron_status',)
+#     search_fields = ('user',)
