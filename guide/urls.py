a283e9bbfb0c17e5d172a18d7345df0eb7ee7a47
Cillian G <cilliangoan221@gmail.com>
2022-09-18 06:36:43 +0000

    Fix 'line too long error' in python

--- guide/urls.py
+++ guide/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', views.PreviewList.as_view(), name='home'),
     path('directory/', views.LocationList.as_view(), name='directory'),
     path('<slug:slug>/', views.LocationDetails.as_view(), name='location'),
-    path('bookmark/<slug:slug>/', views.LocationBookmark.as_view(), name='location_bookmark'),
-    path('bookmarks', views.LocationBookmark.as_view(), name='bookmarks'),
-    # path('upgrade', views.LocationList.as_view(), name='upgrade'),
-    
+    path(
+        'bookmark/<slug:slug>/', views.LocationBookmark.as_view(),
+        name='location_bookmark'
+        ),
 ]
