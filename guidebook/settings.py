39820c2a3c0ea2cdedda18353a804e6f14aa5269
Cillian G <cilliangoan221@gmail.com>
2022-10-09 19:59:19 +0000

    Add messages to base template

--- guidebook/settings.py
+++ guidebook/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -59,6 +60,15 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 # Is this necessary or does crispyforms now have a bootstrap5 template pack? 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
