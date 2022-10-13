cb63e53aeb48aa7e580118e5c2343f1e4532f264
Cillian G <cilliangoan221@gmail.com>
2022-10-13 16:11:04 +0000

    Fix email verification bug in registration process

--- guidebook/settings.py
+++ guidebook/settings.py
@@ -60,7 +60,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_REQUIRED = False
 
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-info',
