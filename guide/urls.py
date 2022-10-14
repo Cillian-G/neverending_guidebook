2175c543daf3b0d947defe048f0ff937d4a7e517
Cillian G <cilliangoan221@gmail.com>
2022-10-14 10:50:31 +0000

    Comment out bookmarks url for future implementation

--- guide/urls.py
+++ guide/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.PreviewList.as_view(), name='home'),
     path('directory/', views.LocationList.as_view(), name='directory'),
+    # path('bookmarks/', views.BookmarkList.as_view(), name='bookmarks'),
     path('<slug:slug>/', views.LocationDetails.as_view(), name='location'),
     path(
         'bookmark/<slug:slug>/', views.LocationBookmark.as_view(),
