eb9889816b1fbfeba02de9f6f6e2c9be0365f5ff
Cillian G <cilliangoan221@gmail.com>
2022-11-21 15:55:13 +0000

    Fix JS8 validation errors

--- static/js/stripe_elements.js
+++ static/js/stripe_elements.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 11, jquery: true */
+
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var stripe_secret_key = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
