487d5def4d06527afb8df26acdf0476aaba9bb4c
Cillian G <cilliangoan221@gmail.com>
2022-11-21 10:51:45 +0000

    Add url path to add, edit and delete locations

--- guide/urls.py
+++ guide/urls.py
@@ -6,6 +6,13 @@ urlpatterns = [
     path('', views.PreviewList.as_view(), name='home'),
     path('directory/', views.LocationList.as_view(), name='directory'),
     # path('bookmarks/', views.BookmarkList.as_view(), name='bookmarks'),
+    path('location/add/', views.add_location, name='add_location'),
+    path(
+        'location/edit/<int:location_id>',
+        views.edit_location, name='edit_location'),
+    path(
+        'location/delete/<int:location_id>',
+        views.delete_location, name='delete_location'),
     path('<slug:slug>/', views.LocationDetails.as_view(), name='location'),
     path(
         'bookmark/<slug:slug>/', views.LocationBookmark.as_view(),
