eb88b92cbe0929c80a7ec2917108b5ff91171027
Cillian G <cilliangoan221@gmail.com>
2022-11-21 12:32:57 +0000

    Add 404 handler to guidebook urls

--- guidebook/urls.py
+++ guidebook/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('guide.urls'), name='guide_urls'),
     path('accounts/', include('allauth.urls')),
-    
 ]
+
+handler404 = 'boutique_ado.views.handler404'
