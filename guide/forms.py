54923ccade1d42c73e498ad0afe45008a5840da2
Cillian G <cilliangoan221@gmail.com>
2022-11-21 10:47:22 +0000

    Create form for adding and editing locations

--- guide/forms.py
+++ guide/forms.py
@@ -1,7 +1,14 @@
-from .models import Region
 from django import forms
+from .models import Region, Location
+
 
 class RegionForm(forms.ModelForm):
     class Meta:
         model = Region
         fields = ('name',)
+
+
+class LocationForm(forms.ModelForm):
+    class Meta:
+        model = Location
+        fields = ('title', 'slug', 'country', 'content', 'image')
